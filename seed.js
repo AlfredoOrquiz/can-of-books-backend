1db225f644e66d479f50a054eba3fd25f08ae50c
Carlos A. Herrera <cherrera1208@gmail.com>
2022-08-30 17:46:33 -0700

    lab12 server setup to add and delete books

--- seed.js
+++ seed.js
@@ -7,24 +7,21 @@ const Books = require('./models/books.js');
 
 
 async function seed() {
-  // title: { type: String, require: true },
-  // description: { type: String, require: true },
-  // status: { type: String, require: true }
   await Books.create({
-    title: 'Blood Meridian',
-    description: 'Based on historical event that took place on the Texas-Mexico border in the 1850s, Blood Meridian is an epic novel that traces the fortunes of the Kid, a fourteen-year-old Tennesseean who into a nightmarish world in which the market for Indian scalps is thriving',
-    status: 'read'
+    title: 'Hackers',
+    description: 'Steven Levy\'s classic book traces the exploits of the computer revolution\'s original hackers - those brilliant and eccentric nerds from the late 1950s through the early \'80s who took risks, bent the rules, and pushed the world in a radical new direction. With updated material from noteworthy hackers such as Bill Gates, Mark Zuckerberg, Richard Stallman, and Steve Wozniak, Hackers is a fascinating story that begins in early computer research labs and leads to the first home computers.',
+    status: 'Reading'
   });
-  console.log('book1 added');
+  console.log('book added');
   await Books.create({
-    title: 'Ficciones',
-    description: 'Borges sends us on a journey into a compelling, bizarre, and profoundly resonant realm; we enter the fearful sphere of Pascal\'s abyss, the surreal and literal labyrinth of books, and the iconography of eternal return. More playful and approachable than the fictions themselves are Borges\'s Prologues, brief elucidations that offer the uninitiated a passageway into the whirlwind of Borges\'s genius and mirror the precision and potency of his intellect and inventiveness, his piercing irony, his skepticism, and his obsession with fantasy. To enter the worlds in Ficciones is to enter the mind of Jorge Luis Borges, wherein lies Heaven, Hell, and everything in between.',
-    status: 'status2'
+    title: 'Don Quijote',
+    description: 'Don Quixote has become so entranced reading tales of chivalry that he decides to turn knight errant himself. In the company of his faithful squire, Sancho Panza, these exploits blossom in all sorts of wonderful ways. While Quixote\'s fancy often leads him astray—he tilts at windmills, imagining them to be giants—Sancho acquires cunning and a certain sagacity. Sane madman and wise fool, they roam the world together-and together they have haunted readers\' imaginations for nearly four hundred years.',
+    status: 'Want To Read'
   });
   await Books.create({
     title: 'Moby Dick',
     description: ' "Call me Ishmael." Thus starts the greatest American novel. Melville said himself that he wanted to write "a mighty book about a mighty theme" and so he did. It is a story of one man\'s obsessive revenge-journey against the white whale, Moby-Dick, who injured him in an earlier meeting. Woven into the story of the last journey of The Pequod is a mesh of philosophy, rumination, religion, history, and a mass of information about whaling through the ages. ',
-    status: 'status3'
+    status: 'Read'
   });
   mongoose.disconnect();
 }
